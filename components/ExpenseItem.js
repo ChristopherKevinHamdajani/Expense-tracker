6be08a684a0e16ab027dd1df6c1ac7e8f4d90d5f
ckh35 <ckh35@uclive.ac.nz>
2021-11-17 21:03:09 +1300

    build more complex components

--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -1,4 +1,14 @@
+import './ExpenseItem.css'
+
 function ExpenseItems() {
-    return <h2>Your Expenses!</h2>
+    return (
+        <div className = "expense-item">
+            <div>November 17th 2021</div>
+            <div className = "expense-item__description">
+                <h2>Gaming Laptop</h2>
+                <div className = "expense-item__price">$2199</div>
+            </div>
+        </div>
+    );
 }
 export default ExpenseItems;
