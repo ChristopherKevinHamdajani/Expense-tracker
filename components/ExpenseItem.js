a4b9f9f005fa7a153905d9821f48f185fff3f8ca
ckh35 <ckh35@uclive.ac.nz>
2021-11-17 21:53:37 +1300

    using props for components

--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -1,15 +1,12 @@
 import './ExpenseItem.css'
 
-function ExpenseItems() {
-    const expenseName = 'Macbook Laptop';
-    const expenseDate = new Date(2021,10,17);
-    const expensePrice = 2199;
+function ExpenseItems(props) {
     return (
         <div className = "expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className = "expense-item__description">
-                <h2>{expenseName}</h2>
-                <div className = "expense-item__price">${expensePrice}</div>
+                <h2>{props.title}</h2>
+                <div className = "expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
